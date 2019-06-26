6fbd610d644733f47e82b232062037ddfbbb2aff
Sharanagouda <Sharanu.mdl@gmail.com>
2019-06-26 11:36:04 +0530

    action types

---
+++ src/actionTypes/index.js
@@ -0,0 +1,26 @@
+//Authentication
+export const AUTH_LOGIN_USER = "AUTH_LOGIN_USER";
+export const AUTH_LOGIN_USER_FAIL = "AUTH_LOGIN_USER_FAIL";
+export const AUTH_LOGIN_USER_SUCCESS =  "AUTH_LOGIN_USER_SUCCESS";
+
+export const AUTH_CREATE_USER = "CREATE_USER";
+export const AUTH_CREATE_USER_FAIL = "AUTH_CREATE_USER_FAIL";
+export const AUTH_CREATE_USER_SUCCESS  = "AUTH_CREATE_USER_SUCCESS";
+
+//Post types
+export const POST_FETCH_ALL = "POST_FETCH_ALL";
+export const POST_ADD = "POST_ADD";
+export const POST_SELECT_IMAGE = "POST_SELECT_IMAGE";
+export const POST_LIKE = "POST_LIKE";
+export const POST_SHARE = "POST_SHARE";
+export const POST_DISLIKE = "POST_DISLIKE";
+export const POST_ADD_COMMENT = "POST_ADD_COMMENT";
+
+//Profile types
+export const PROFILE = "PROFILE_FETCH";
+export const PROFILE_EDIT = "PROFILE_EDIT";
+
+//Hightlights
+export const HIGHLIGHT_FETCH_ALL = "HIGHLIGHT_FETCH_ALL";
+export const HIGHLIGHT_CREATE = "HIGHLIGHT_CREATE";
+
